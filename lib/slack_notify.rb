ff399e794fe25842a838b623b624ffac2cad86f1
Eric Gowens <eric.gowens@gmail.com>
2015-09-25 11:12:03 -0700

    Fixed slack notifications

--- lib/slack_notify.rb
+++ lib/slack_notify.rb
@@ -5,10 +5,7 @@ class SlackNotify
     payload = {
       text: "A new shop has installed Touchcart: #{domain}"
     }
-    #send_to_slack(payload)
-    @slack_url = "https://hooks.slack.com/services/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"
-    resp = RestClient.post(@slack_url, payload.to_json)
-    resp.code
+    send_to_slack(payload)
   end
 
   def self.uninstall(domain)
@@ -28,9 +25,9 @@ class SlackNotify
 
   private
 
-  def send_to_slack(payload)
+  def self.send_to_slack(payload)
     @slack_url = "https://hooks.slack.com/services/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"
-    resp = RestClient.post(slack_url, payload.to_json)
+    resp = RestClient.post(@slack_url, payload.to_json)
     resp.code
   end
 end
