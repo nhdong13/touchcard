9165ee721184b666fa6b11870a1546b231a20ac7
Eric Gowens <eric.gowens@gmail.com>
2015-10-15 23:56:43 -0600

    Changed webhook verification logic

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -69,7 +69,7 @@ class WebhookController < AuthenticatedController
     data = request.body.read.to_s
     hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
     digest  = OpenSSL::Digest::Digest.new('sha256')
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, Touchcard::Application.config.shopify.secret, data)).strip
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, ShopifyApp.configuration.secret, data)).strip
     unless calculated_hmac == hmac_header
       head :unauthorized
     end
