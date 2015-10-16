c62736e523aabede60656ef78921447f832741a6
Eric Gowens <eric.gowens@gmail.com>
2015-10-16 00:26:54 -0600

    skip devise rails 4 style

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,5 +1,5 @@
 class WebhookController < AuthenticatedController
-  skip_before_filter  :verify_authenticity_token, only: [:new_orer, :uninstall]
+  skip_before_action :verify_authenticity_token
   before_filter :verify_webhook, only: [:new_order, :uninstall]
 
   def new_order
