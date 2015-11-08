ae4e57f8f316a1dc1c727c15684fe5bbb1857006
Eric Gowens <eric.gowens@gmail.com>
2015-11-08 10:28:58 -0600

    Shop migration to change from up-down

--- db/migrate/20150904194814_add_attributes_to_shop.rb
+++ db/migrate/20150904194814_add_attributes_to_shop.rb
@@ -1,5 +1,5 @@
 class AddAttributesToShop < ActiveRecord::Migration
-  def up
+  def change
     add_column :shops, :shopify_id,     :bigint
     add_column :shops, :credit,         :int, default: 0
     add_column :shops, :webhook_id,     :bigint
@@ -11,19 +11,5 @@ class AddAttributesToShop < ActiveRecord::Migration
     add_column :shops, :last_month,     :int
     add_column :shops, :send_next,      :boolean, null: false, default: true
     add_column :shops, :last_login,     :datetime
-  end
-
-  def down
-    remove_column :shops, :shopify_id
-    remove_column :shops, :credit
-    remove_column :shops, :webhook_id
-    remove_column :shops, :uninstall_id
-    remove_column :shops, :charge_id
-    remove_column :shops, :charge_amount
-    remove_column :shops, :charge_date
-    remove_column :shops, :customer_pct
-    remove_column :shops, :last_month
-    remove_column :shops, :send_next
-    remove_column :shops, :last_login
   end
 end
