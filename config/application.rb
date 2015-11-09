56f79199c0386ed23a6e1d4910effd70ab664c81
Eric Gowens <eric.gowens@gmail.com>
2015-11-08 23:11:41 -0600

    Delayed Jobs for background tasks

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module Touchcard
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Add delayed_job
+    config.active_job.queue_adapter = :delayed_job
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
