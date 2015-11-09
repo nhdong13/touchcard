f7dcda6156312af7aa70855b429d88d97b8ca29a
Eric Gowens <eric.gowens@gmail.com>
2015-11-09 17:14:35 -0500

    Plans retrurns array

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -2,6 +2,6 @@ class API::V1::PlansController < API::BaseController
 
   def index
     @current_shop.get_last_month
-    render json: @current_shop, serializer: ShopSerializer
+    render json: { plans:  (0..4).map { |i|  { amount: @current_shop.last_month * i * 0.25 } } }
   end
 end
