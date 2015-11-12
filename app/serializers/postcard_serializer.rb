5bd1aa4ba08e58e6fbeb9a5adee39606484681c4
Tony <tony@cc.com>
2015-11-12 11:50:42 -0500

    remove attributes that are not supposed to be sent to client

--- app/serializers/postcard_serializer.rb
+++ app/serializers/postcard_serializer.rb
@@ -1,7 +1,7 @@
 class PostcardSerializer < ActiveModel::Serializer
-  attributes :id, :card_order_id, :discount_code, :order_id, :customer_id, :customer_name,
-    :addr1, :addr2, :city, :state, :country, :zip, :send_date, :sent, :date_sent,
-    :postcard_id, :return_customer, :purchase2
-
-  has_one :card_order, embed: :ids
+  attributes :id,
+    :customer_name, :addr1, :addr2, :city, :state, :country, :zip,
+    :send_date, :sent, :date_sent,
+    :return_customer, :purchase2,
+    :card_order_id
 end
