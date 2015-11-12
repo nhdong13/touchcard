c3fb98cc756e2f43568f2ea723ba5db155485acc
Tony <tony@cc.com>
2015-11-12 11:15:53 -0500

    needed parameters sent to the client in shop

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,4 +1,3 @@
 class ShopSerializer < ActiveModel::Serializer
-  attributes :id, :shopify_id, :credit, :webhook_id, :uninstall_id, :charge_id,
-    :charge_amount, :charge_date, :customer_pct, :last_month, :send_next, :last_login
+  attributes :id, :credit, :charge_id
 end
