695b10f23e1bb1169c996db37b2e7aa0e63a0d77
Tony <tony@cc.com>
2015-11-24 18:34:08 -0500

    remove expires for now

--- app/controllers/api/v1/aws_controller.rb
+++ app/controllers/api/v1/aws_controller.rb
@@ -1,36 +1,35 @@
 class Api::V1::AwsController < Api::BaseController
   def sign
-    bucket = Aws::S3::Resource.new.bucket('touchcard-user')
+    bucket = Aws::S3::Resource.new.bucket("touchcard-user")
     @s3_direct_post = bucket.presigned_post(
       key: "uploads/#{SecureRandom.uuid}/${filename}",
-      success_action_status: '201',
-      acl: 'public-read',
-      expires: 10.minutes
+      success_action_status: "201",
+      acl: "public-read"
     )
     render json: @s3_direct_post
   end
   # def sign
   #   @expires = 1.hours.from_now
   #   render json: {
-  #     acl: 'public-read',
-  #     awsaccesskeyid: ENV['AWS_ACCESS_KEY_ID'],
-  #     bucket: 'touchcard-user',
+  #     acl: "public-read",
+  #     awsaccesskeyid: ENV["AWS_ACCESS_KEY_ID"],
+  #     bucket: "touchcard-user",
   #     expires: @expires,
   #     key: "uploads/#{params[:name]}",
   #     policy: policy,
   #     signature: signature,
-  #     success_action_status: '201',
-  #     'Content-Type' => params[:type],
-  #     'Cache-Control' => 'max-age=630720000, public'
+  #     success_action_status: "201",
+  #     "Content-Type" => params[:type],
+  #     "Cache-Control" => "max-age=630720000, public"
   #   }, status: :ok
   # end
   #
   # def signature
   #   Base64.strict_encode64(
   #     OpenSSL::HMAC.digest(
-  #       OpenSSL::Digest::Digest.new('sha1'),
-  #       ENV['AWS_SECRET_ACCESS_KEY'],
-  #       policy(secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+  #       OpenSSL::Digest::Digest.new("sha1"),
+  #       ENV["AWS_SECRET_ACCESS_KEY"],
+  #       policy(secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"])
   #     )
   #   )
   # end
@@ -40,14 +39,14 @@ class Api::V1::AwsController < Api::BaseController
   #     {
   #       expiration: @expires,
   #       conditions: [
-  #         { bucket: 'touchcard-user' },
-  #         { acl: 'public-read' },
+  #         { bucket: "touchcard-user" },
+  #         { acl: "public-read" },
   #         { expires: @expires },
-  #         { success_action_status: '201' },
-  #         [ 'starts-with', '$key', '' ],
-  #         [ 'starts-with', '$Content-Type', '' ],
-  #         [ 'starts-with', '$Cache-Control', '' ],
-  #         [ 'content-length-range', 0, 524288000 ]
+  #         { success_action_status: "201" },
+  #         [ "starts-with", "$key", "" ],
+  #         [ "starts-with", "$Content-Type", "" ],
+  #         [ "starts-with", "$Cache-Control", "" ],
+  #         [ "content-length-range", 0, 524288000 ]
   #       ]
   #     }.to_json
   #   )
