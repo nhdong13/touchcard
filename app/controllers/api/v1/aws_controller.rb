31ff2a84044de1d580d3c3c48ae6852e15809064
Tony <tony@cc.com>
2015-11-24 18:30:51 -0500

    try a different way of signing

--- app/controllers/api/v1/aws_controller.rb
+++ app/controllers/api/v1/aws_controller.rb
@@ -1,45 +1,55 @@
 class Api::V1::AwsController < Api::BaseController
   def sign
-    @expires = 1.hours.from_now
-    render json: {
-      acl: 'public-read',
-      awsaccesskeyid: ENV['AWS_ACCESS_KEY_ID'],
-      bucket: 'touchcard-user',
-      expires: @expires,
-      key: "uploads/#{params[:name]}",
-      policy: policy,
-      signature: signature,
+    bucket = Aws::S3::Resource.new.bucket('touchcard-user')
+    @s3_direct_post = bucket.presigned_post(
+      key: "uploads/#{SecureRandom.uuid}/${filename}",
       success_action_status: '201',
-      'Content-Type' => params[:type],
-      'Cache-Control' => 'max-age=630720000, public'
-    }, status: :ok
-  end
-
-  def signature
-    Base64.strict_encode64(
-      OpenSSL::HMAC.digest(
-        OpenSSL::Digest::Digest.new('sha1'),
-        ENV['AWS_SECRET_ACCESS_KEY'],
-        policy(secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
-      )
+      acl: 'public-read',
+      expires: 10.minutes
     )
+    render json: @s3_direct_post
   end
-
-  def policy(options = {})
-    Base64.strict_encode64(
-      {
-        expiration: @expires,
-        conditions: [
-          { bucket: 'touchcard-user' },
-          { acl: 'public-read' },
-          { expires: @expires },
-          { success_action_status: '201' },
-          [ 'starts-with', '$key', '' ],
-          [ 'starts-with', '$Content-Type', '' ],
-          [ 'starts-with', '$Cache-Control', '' ],
-          [ 'content-length-range', 0, 524288000 ]
-        ]
-      }.to_json
-    )
-  end
+  # def sign
+  #   @expires = 1.hours.from_now
+  #   render json: {
+  #     acl: 'public-read',
+  #     awsaccesskeyid: ENV['AWS_ACCESS_KEY_ID'],
+  #     bucket: 'touchcard-user',
+  #     expires: @expires,
+  #     key: "uploads/#{params[:name]}",
+  #     policy: policy,
+  #     signature: signature,
+  #     success_action_status: '201',
+  #     'Content-Type' => params[:type],
+  #     'Cache-Control' => 'max-age=630720000, public'
+  #   }, status: :ok
+  # end
+  #
+  # def signature
+  #   Base64.strict_encode64(
+  #     OpenSSL::HMAC.digest(
+  #       OpenSSL::Digest::Digest.new('sha1'),
+  #       ENV['AWS_SECRET_ACCESS_KEY'],
+  #       policy(secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+  #     )
+  #   )
+  # end
+  #
+  # def policy(options = {})
+  #   Base64.strict_encode64(
+  #     {
+  #       expiration: @expires,
+  #       conditions: [
+  #         { bucket: 'touchcard-user' },
+  #         { acl: 'public-read' },
+  #         { expires: @expires },
+  #         { success_action_status: '201' },
+  #         [ 'starts-with', '$key', '' ],
+  #         [ 'starts-with', '$Content-Type', '' ],
+  #         [ 'starts-with', '$Cache-Control', '' ],
+  #         [ 'content-length-range', 0, 524288000 ]
+  #       ]
+  #     }.to_json
+  #   )
+  # end
 end
