073d3900d7da2b51eae19ec840c3e9bfb4df1989
Tony <tony@cc.com>
2015-11-25 10:13:30 -0500

    time at

--- app/controllers/api/v1/shopify_customers_controller.rb
+++ app/controllers/api/v1/shopify_customers_controller.rb
@@ -3,8 +3,8 @@ require 'customer_check'
 class Api::V1::ShopifyCustomersController < Api::BaseController
   def count
     @count = CustomerCheck.get_customer_number(@current_shop,
-      params[:created_after],
-      params[:created_before])
+      Time.at(params[:created_after]),
+      Time.at(params[:created_before]))
     render json: { meta: { count: @count} }, status: 200
   end
 end
