94efaa1da3484cf4f544851cad3dc234c608f6f1
Tony <tony@cc.com>
2015-11-25 10:15:40 -0500

    parse to integer first

--- app/controllers/api/v1/shopify_customers_controller.rb
+++ app/controllers/api/v1/shopify_customers_controller.rb
@@ -3,8 +3,8 @@ require 'customer_check'
 class Api::V1::ShopifyCustomersController < Api::BaseController
   def count
     @count = CustomerCheck.get_customer_number(@current_shop,
-      Time.at(params[:created_after]),
-      Time.at(params[:created_before]))
+      Time.at(Integer(params[:created_after])),
+      Time.at(Integer(params[:created_before])))
     render json: { meta: { count: @count} }, status: 200
   end
 end
