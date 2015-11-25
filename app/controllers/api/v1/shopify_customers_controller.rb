e8a45c58ee1b5f658869c16eaa1d84940a2a56ff
Tony <tony@cc.com>
2015-11-25 09:21:44 -0500

    return count as a meta object

--- app/controllers/api/v1/shopify_customers_controller.rb
+++ app/controllers/api/v1/shopify_customers_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::ShopifyCustomersController < Api::BaseController
     @count = get_customer_number(@current_shop,
       params[:created_after],
       params[:created_before])
-    render json: @count
+    render json: { meta: { count: @count} }
   end
 end
