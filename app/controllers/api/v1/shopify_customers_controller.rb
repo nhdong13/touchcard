e9c9d269bab0c2bf2f3287a39811840d3e752491
Tony <tony@cc.com>
2015-11-26 17:44:35 -0500

    js sends dates 1000 times what ruby expects

--- app/controllers/api/v1/shopify_customers_controller.rb
+++ app/controllers/api/v1/shopify_customers_controller.rb
@@ -3,8 +3,8 @@ require 'customer_check'
 class Api::V1::ShopifyCustomersController < Api::BaseController
   def count
     @count = CustomerCheck.get_customer_number(@current_shop.id,
-      Time.at(Integer(params[:created_after])),
-      Time.at(Integer(params[:created_before])))
+      Time.at(Integer(params[:created_after])/1000),
+      Time.at(Integer(params[:created_before])/1000))
     render json: { meta: { count: @count} }, status: 200
   end
 end
