ab975e366a8afeccc1a5a291aaf5315227100305
Tony <tony@cc.com>
2015-11-26 17:08:05 -0500

    try out shop.id instead of just current shop

--- app/controllers/api/v1/shopify_customers_controller.rb
+++ app/controllers/api/v1/shopify_customers_controller.rb
@@ -2,7 +2,7 @@ require 'customer_check'
 
 class Api::V1::ShopifyCustomersController < Api::BaseController
   def count
-    @count = CustomerCheck.get_customer_number(@current_shop,
+    @count = CustomerCheck.get_customer_number(@current_shop.id,
       Time.at(Integer(params[:created_after])),
       Time.at(Integer(params[:created_before])))
     render json: { meta: { count: @count} }, status: 200
