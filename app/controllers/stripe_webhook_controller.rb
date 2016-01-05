da810a33798a8ec60b99c78989708d51aadc924a
Tony <tony@cc.com>
2016-01-05 08:44:54 -0500

    remove csrf

--- app/controllers/stripe_webhook_controller.rb
+++ app/controllers/stripe_webhook_controller.rb
@@ -1,4 +1,5 @@
-class StripeWebhookController < ApplicationController
+class StripeWebhookController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   before_action :set_stripe_event, only: :create
 
   def create
