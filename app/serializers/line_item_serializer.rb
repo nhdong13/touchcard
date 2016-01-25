0feae14fa910561ff41a66a76cb4ac5b7db8cfb7
Tony <tony@cc.com>
2016-01-25 16:17:47 -0500

    add the name into the serializer

--- app/serializers/line_item_serializer.rb
+++ app/serializers/line_item_serializer.rb
@@ -1,5 +1,5 @@
 class LineItemSerializer < ActiveModel::Serializer
-  attributes :id, :order_id, :price, :quantity, :title, :customer_name, :created_at
+  attributes :id, :order_id, :price, :quantity, :name, :customer_name, :created_at
 
   def customer_name
     object.order.customer.try(:name)
