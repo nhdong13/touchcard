2e0bca02b366e834ff832c9073251fb10ce8f968
Tony <tony@cc.com>
2016-01-25 16:18:30 -0500

    full name not name

--- app/serializers/line_item_serializer.rb
+++ app/serializers/line_item_serializer.rb
@@ -2,6 +2,6 @@ class LineItemSerializer < ActiveModel::Serializer
   attributes :id, :order_id, :price, :quantity, :name, :customer_name, :created_at
 
   def customer_name
-    object.order.customer.try(:name)
+    object.order.customer.try(:full_name)
   end
 end
