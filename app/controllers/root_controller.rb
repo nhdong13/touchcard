52cf36e3e1300ea85c95dadcd97418ddb042e9df
dixpac <dino.onex@gmail.com>
2016-04-07 12:34:43 +0200

    Enable rails and ember to work locally
    * In development we are bootstraping index from public/index.html
      and that is our ember app. Runing ember serve or ember build --watch
      wil output build files in public/client automatically, there files
      are refernced in public/index.html.
      PLEASE DON'T CHANGE THESE PATHS UNLESS THERE IS GOOD REASSON FOR IT.
    * Also added some comments to root_controller for easier understanig of
      process and remove commente code from befre
    * public/client is added to .gitignore since this is files that are
      builded locally
    * added .irbhistory to .gitignore this file is automatically generated
      by BYEBUG debugger, and we don't need him in repo

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,20 +1,25 @@
 class RootController < AuthenticatedController
+  # get the redis cloud connection
   def redis
     uri = URI.parse(ENV['REDISCLOUD_URL'])
     @redis ||= Redis.new(host: uri.host, port: uri.port, password: uri.password)
   end
 
+  # WARNING: PELASE DON'T CHNAGE THIS WITHOUT GOOD REASON
+  # this bootstraps ember through rails, so we can avoid CORS problems
+  # if we are in development environemt load local ember assets, else
+  # load from our redis in cloud
   def bootstrap_index(index_key, app)
-    return File.read("#{Rails.root}/private/index.html") unless Rails.env.production?
+    return File.read("#{Rails.root}/public/index.html") unless Rails.env.production?
     index_key = params[:index_key] || redis.get("#{app}:index:current")
     redis.get("#{app}:index:#{index_key}")
   end
 
   def index
     render layout: "embedded_app"
-  #   redirect_to '/app'
   end
 
+  # dynamically setup shopify variables
   def app
     text = bootstrap_index(params[:index_key], 'touchcard-app')
     text.gsub!("https://{@shop_session.url}", @shop_session ? "https://#{@shop_session.url}" : "")
