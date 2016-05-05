ec6a10dced9582e621fe5a5bbb53e38c5bd453a2
dixpac <dino.onex@gmail.com>
2016-05-05 14:34:49 +0200

    Handle s3 special characters
    * Escapse s3 special characters
    * If name is empty after escaping generate random name
    * I also added loagin bucket name from ENV so upload can work
      in multiple environments.Production bucket name was `touchcard-user`

---
+++ spec/lib/upload_sanitizer_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+require "upload_sanitizer"
+
+RSpec.describe UploadSanitizer do
+
+  describe "#file_name" do
+    it "replaces all s3 special characters with string empty" do
+      name = "test%$&=@:+,?{}\[]<>#^`.jpg"
+
+      sanitizer = UploadSanitizer.new(name)
+
+      expect(sanitizer.new_name).to eq "test.jpg"
+    end
+
+    it "generates random name, name is blank after replace" do
+      name = "%$&=@:+,?{}\[]<>#^`.png"
+
+      sanitizer = UploadSanitizer.new(name)
+      expect(sanitizer.new_name.length).to eq 14
+    end
+  end
+end
