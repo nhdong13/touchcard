dc4c506d3d5d77de147e20a4f545afe1e55c0b02
Dustin Laverick <dustin@laverick.org>
2016-07-14 16:57:33 +0200

    Skip migration in case all discounts aren't there. (Ruffneck scarves... you naughty people.)

--- db/migrate/20160714143931_update_postcard_discount_details.rb
+++ db/migrate/20160714143931_update_postcard_discount_details.rb
@@ -2,6 +2,7 @@ class UpdatePostcardDiscountDetails < ActiveRecord::Migration
   def up
     postcards = Postcard.where(sent: true)
     postcards.each do |p|
+      next if not p.card_order.discount?
       exp_at = p.estimated_arrival + p.card_order.discount_exp.weeks
       p.update_attributes(discount_pct: p.card_order.discount_pct,
                           discount_exp_at: exp_at)
