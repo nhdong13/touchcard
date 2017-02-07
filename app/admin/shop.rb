d569a175b329264a5db4ebbe63ea360ea6b5d536
JovicaSusa <jovicasusa1985@gmail.com>
2017-02-07 22:46:07 +0100

    Added active admin index and show page for shop
    * Dashboard page cleared for now

---
+++ app/admin/shop.rb
@@ -0,0 +1,54 @@
+ActiveAdmin.register Shop do
+  includes :subscriptions
+
+  index do
+    column :id
+    column :domain
+    column :created_at
+    column :updated_at
+    column :uninstalled_at
+    column :last_login_at
+    column :email
+    column :customer_email
+    column :plan_name
+    column :owner
+    column :name
+    column :approval_state
+    column :last_month
+    column :credit
+    column "Subscription Quantity" do |shop|
+      shop.current_subscription.quantity if shop.current_subscription
+    end
+    actions
+  end
+
+  show do
+    attributes_table :domain, :email, :owner, :name
+
+    panel "Subscription Data" do
+      table_for shop.subscriptions do
+        column :id
+        column :quantity
+        column :plan_id
+        column :shop_id
+        column :current_period_start
+        column :current_period_end
+        column :created_at
+        column :updated_at
+        column :stripe_id
+      end
+    end
+
+    panel "Relevant Card Orders" do
+      table_for shop.card_orders do
+        column  :id
+        column  :discount_pct
+        column  :discount_exp
+        column  :enabled
+        column :card_side_front_id
+        column :card_side_back_id
+      end
+    end
+    active_admin_comments
+  end
+end
