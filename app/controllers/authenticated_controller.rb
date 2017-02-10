47e0a2f8bc728cf745373dc0d6824c35db9a0793
JovicaSusa <jovicasusa1985@gmail.com>
2017-02-10 19:43:23 +0100

    Fix changed code for update shopify app gem

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticatedController < ApplicationController
-  before_action :login_again_if_different_shop, only: [:second_login]
-  around_action :shopify_session, only: [:index]
+  before_action :login_again_if_different_shop
+  around_action :shopify_session
   layout ShopifyApp.configuration.embedded_app? ? "embedded_app" : "application"
 end
