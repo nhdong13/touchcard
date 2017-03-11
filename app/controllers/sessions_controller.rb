bb33c8bdd82b437fca1849bc527977132f565e76
Dustin Laverick <dustin@laverick.org>
2017-03-11 18:35:57 -0300

    Use shopify_app default controllers, update inheritance, 7.2.5 tidy ups

--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < ApplicationController
-  include ShopifyApp::SessionsConcern
-  include ShopifyApp::LoginProtection
-  # def return_address
-  #   session.delete(:return_to) || '/app'
-  # end
-end
