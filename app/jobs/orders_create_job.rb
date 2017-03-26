25f5782bffa483c5b7b3787c121bab467dc2f906
JovicaSusa <jovicasusa1985@gmail.com>
2017-03-26 18:55:05 +0200

    Fixed new webhooks active jobs
    * Added Shop#with_shopify_session method for handling webhook jobs

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -1,37 +1,34 @@
 class OrdersCreateJob < ActiveJob::Base
-  queue_as :default
-
+  
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(domain: shop_domain)
     shop.with_shopify_session do
-      shopify_orders = webhook[:orders]
-      shopify_orders.each do |shopify_order|
-        order = Order.from_shopify!(shopify_order, shop)
-        order.connect_to_postcard
-        return logger.info "no customer" unless order.customer
-        return logger.info "no default address" unless shopify_order.customer.respond_to?(:default_address)
-        return logger.info "no default address" unless shopify_order.customer.default_address
-        default_address = shopify_order.customer.default_address
-        international = default_address.country_code != "US"
+      shopify_order = ShopifyAPI::Order.find(webhook["id"])
+      order = Order.from_shopify!(shopify_order, shop)
+      order.connect_to_postcard
+      return Delayed::Worker.logger.debug("no customer") unless order.customer
+      return Delayed::Worker.logger.debug("no default address") unless shopify_order.customer.respond_to?(:default_address)
+      return Delayed::Worker.logger.debug("no default address") unless shopify_order.customer.default_address
+      default_address = shopify_order.customer.default_address
+      international = default_address.country_code != "US"
 
-        # Only new customers recieve postcards at the moment
-        return logger.info "Not a new customer" unless order.customer.new_customer?
-        # Create a new card and schedule to send
-        post_sale_order = shop.card_orders.find_by(
-          enabled: true,
-          type: "PostSaleOrder")
-        return logger.info "Card not setup" if post_sale_order.nil?
-        return logger.info "Card not enabled" unless post_sale_order.enabled?
-        return logger.info "international customer not enabled" if international && !post_sale_order.international?
-        return logger.info "order filtered out" unless post_sale_order.send_postcard?(order)
-        postcard = Postcard.new(
-          customer: order.customer,
-          order: order,
-          card_order: post_sale_order,
-          send_date: post_sale_order.send_date,
-          paid: false)
-        postcard.pay.save! if postcard.can_afford?
-      end
+      # Only new customers recieve postcards at the moment
+      return Delayed::Worker.logger.debug( "Not a new customer") unless order.customer.new_customer?
+      # Create a new card and schedule to send
+      post_sale_order = shop.card_orders.find_by(
+        enabled: true,
+        type: "PostSaleOrder")
+      return Delayed::Worker.logger.debug("Card not setup") if post_sale_order.nil?
+      return Delayed::Worker.logger.debug("Card not enabled") unless post_sale_order.enabled?
+      return Delayed::Worker.logger.debug("international customer not enabled") if international && !post_sale_order.international?
+      return Delayed::Worker.logger.debug("order filtered out") unless post_sale_order.send_postcard?(order)
+      postcard = Postcard.new(
+        customer: order.customer,
+        order: order,
+        card_order: post_sale_order,
+        send_date: post_sale_order.send_date,
+        paid: false)
+      postcard.pay.save! if postcard.can_afford?
     end
   end
 end
