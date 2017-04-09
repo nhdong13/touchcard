b16d42c9ecdb03ba088f6a29c3e12e23ccc517f4
Dustin Laverick <dustin@laverick.org>
2017-04-09 15:03:29 -0300

    Fix broken test for postcard estimates, which were updated recently.

--- spec/models/postcard_spec.rb
+++ spec/models/postcard_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Postcard, type: :model do
       expect(postcard.sent).to eq true
       expect(postcard.date_sent).to eq Date.today
       # XXX: because comparison is to noon small probability of failure if run exactly at midnight
-      expect(postcard.estimated_arrival.noon).to eq 5.business_days.from_now.noon
+      expect(postcard.estimated_arrival.noon).to eq 6.business_days.from_now.noon
       expect(postcard.postcard_id).to eq "psc_14c1b66f31264c34"
     end
   end
