6260b4df781c87098c3ef5a24ad400c62675bfb0
Jovica Susa <jovicasusa1985@gmail.com>
2017-05-22 20:52:14 +0200

    Added Postcard active admin page

---
+++ app/admin/postcard.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Postcard do
+  actions :index, :show
+
+  index do
+    div class: 'top_pagination' do
+      paginated_collection(collection, download_links: false)
+    end
+
+    column :discount_code
+    column :send_date
+    column :sent
+    column :date_sent
+    column :created_at
+    column :updated_at
+    column :paid
+    column :estimated_arrival
+    column :arrival_notification_sent
+    column :expiration_notification_sent
+    column :discount_pct
+    column :discount_exp_at
+    column :canceled
+
+    actions
+  end
+
+  show do
+    attributes_table :discount_code,
+                     :send_date,
+                     :sent,
+                     :date_sent,
+                     :created_at,
+                     :updated_at,
+                     :paid,
+                     :estimated_arrival,
+                     :arrival_notification_sent,
+                     :expiration_notification_sent,
+                     :discount_pct,
+                     :discount_exp_at,
+                     :canceled
+  end
+
+end
