fcb04ad1455380a7f6b635220e9e885382ed37c4
Jovica Susa <jovicasusa1985@gmail.com>
2017-05-23 17:39:01 +0200

    WIP - Added change subscription quantity from active admin

--- app/admin/subscription.rb
+++ app/admin/subscription.rb
@@ -1,12 +1,18 @@
 ActiveAdmin.register Subscription do
   actions :index, :show
 
+  member_action :change_quantity, method: :get do
+
+  end
+
   index do
     column :id
     column :quantity
     column :current_period_start
     column :current_period_end
-    actions
+    actions do |subscription|
+      link_to "Change Quantity", change_quantity_admin_subscription_path(subscription)
+    end
   end
 
   show do
