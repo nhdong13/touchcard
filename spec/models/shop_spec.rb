b767d9916a1c12618d612fc49901af60c4ebdb8c
Jovica Susa <jovicasusa1985@gmail.com>
2017-06-12 16:33:11 +0200

    Added specs for shop scopes

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
+  describe "keeps oauth scopes up to date" do
+    let(:shop) { create(:shop) }
+    let(:shopify_scope) { ShopifyApp.configuration.scope }
+
+    it "detects that scopes are not up to date" do
+      expect(shop.granted_scopes_match?(shopify_scope)).to be_falsy
+    end
+
+    it "detects that scopes are up to date" do
+      shop.oauth_scopes = shopify_scope
+      expect(shop.granted_scopes_match?(shopify_scope)).to be_truthy
+    end
+
+    it "can update scopes" do
+      expect(shop.oauth_scopes).to be_nil
+      shop.update_scopes(shopify_scope)
+      expect(shop.oauth_scopes).to eq(shopify_scope)
+    end
+  end
 end
