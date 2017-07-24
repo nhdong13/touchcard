8c6821edf89bffbe74437c611de83110f4bf9d2a
Jovica Susa <jovicasusa1985@gmail.com>
2017-07-24 11:53:38 +0200

    Modify handling customer winback postcards

--- app/services/customer_winback_handler.rb
+++ app/services/customer_winback_handler.rb
@@ -1,70 +1,27 @@
 class CustomerWinbackHandler
-  # Call this from daily task
-  attr_reader :shop
+  attr_reader :customer
 
-  def initialize(shop)
-    @shop = shop
+  def initialize(customer)
+    @customer = customer
   end
 
   def call
-    return unless activate_session
-    pages = (customers_count / 250.0).ceil
-    card = shop.card_orders.find_by(enabled: true, type_name: "Customer Winback")
-    customers = []
-    1.upto(pages) { |page| customers << ShopifyAPI::Customer.find(:all, params: { page: page, limit: 250 }) }
-    customers.each do |customer|
-      # maybe create ShopifyCustomer model since every method needs customer as arg
-      next if orders_for_last_30days(customer) > 0
-      # Fix logic here
-      customer_id = customer.first.id
-      if customer_has_postcard_created(customer_id)
-        if sixty_day_orders(customer_id) > 0
-          create_postcard if time_for_60days_postcard?
-        end
-      elsif sixty_day_orders(customer_id) < 0
-        postcard = Postcard.new(
-          customer: customer_id,
-          card_order: card,
-          send_date: card.send_date,
-          paid: false)
-        postcard.pay.save if shop.sending_active? && shop.can_afford_postcard?
-      end
-    end
+    sent_winback_postcards_number = Postcard.where("type_name = ? AND customer_id = ?", "Customer Winback", customer.id).count
+    num_of_days_for_sending_postcard = sent_winback_postcards_number + 1 * 30
+    last_order_time = customer.orders.last.created_at # order this by created_at
+    days_from_last_order = (Time.zone.now - last_order_time).to_i
+
+    send_winback_postcard if days_from_last_order > num_of_days_for_sending_postcard
   end
 
-  def activate_session
-    begin
-      session = ShopifyAPI::Session.new(shop.domain, shop.token)
-      ShopifyAPI::Base.activate_session(session)
-    rescue
-      ShopifyAPI::Base.clear_session
-      warn "Can't open session for #{shop.domain}"
-    end
+  def send_winback_postcard
+    shop = customer.shop
+    card = shop.card_orders.where(enabled: true, type_name: "CustomerWinback")
+    Postcard.new(
+      customer: customer,
+      card_order: card,
+      send_date: card.send_date,
+      paid: false)
+    postcard.pay.save! if shop.sending_active? && shop.can_afford_postcard?
   end
-
-  def customers_count
-    ShopifyAPI::Customer.count
-  end
-
-  def orders_for_last_30days(customer_id)
-    @orders_for_last_30days ||= ShopifyAPI::Order.count(customer_id: "#{customer_id}", created_at_min: 1.month.ago.to_date)
-  end
-
-  def sixty_day_orders(customer_id)
-    @sixty_day_orders ||= ShopifyAPI::Order.count(customer_id: "#{customer_id}", created_at_min: 2.month.ago.to_date)
-  end
-
-  def time_for_60days_postcard?
-    # check that postcard last created postcard is created 30 days ago
-    # customer_winback_postcard.first.created_at <=
-  end
-
-  def customer_has_postcard_created(customer)
-    customer_winback_postcard.any?
-  end
-
-  def customer_winback_postcard(customer)
-    Postcard.where("type_name = ? AND customer_id = ?", "Customer Winback", customer_id)
-  end
-
 end
