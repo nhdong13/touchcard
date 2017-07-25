177ed6529914ac64b47564a99490b0ebaf16bf41
Jovica Susa <jovicasusa1985@gmail.com>
2017-07-25 20:01:08 +0200

    WIP - Fix handle customer winback postcard

--- app/services/customer_winback_handler.rb
+++ app/services/customer_winback_handler.rb
@@ -1,17 +1,14 @@
 class CustomerWinbackHandler
-  attr_reader :customer
+  attr_reader :customer, :shop
 
-  def initialize(customer)
+  def initialize(customer, shop)
     @customer = customer
-  end
-
-  def shop
-    customer.shop
+    @shop = shop
   end
 
   def call
     card = shop.card_orders.where(enabled: true, type_name: "CustomerWinback")
-    return if customer.have_winback_postcard_sent?(card)
+    return if card.nil? || customer.have_winback_postcard_sent?(card)
     process_winback_postcard(card) if customer.eligible_for_winback_postcard(card.winback_delay)
   end
 
@@ -19,7 +16,7 @@ class CustomerWinbackHandler
     Postcard.new(
       customer: customer,
       card_order: card,
-      send_date: Time.zone.now + 1, #set this to card.send_delay
+      send_date: Time.zone.now + 1.day, #set this to card.send_delay
       paid: false)
     postcard.pay.save! if postcard.can_afford?
   end
