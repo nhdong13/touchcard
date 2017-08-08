94df68d097c614969585f58c6e91888e0724afce
Jovica Susa <jovicasusa1985@gmail.com>
2017-08-08 14:28:19 +0200

    Changed after rails app update

--- config/spring.rb
+++ config/spring.rb
@@ -1,2 +1,7 @@
-Spring.watch "config/local_env.yml"
-
+# Spring.watch "config/local_env.yml"
+%w(
+  .ruby-version
+  .rbenv-vars
+  tmp/restart.txt
+  tmp/caching-dev.txt
+).each { |path| Spring.watch(path) }
