bc9c8e4a8e7e7d3be6293978c605e225b8870cc1
Dustin Laverick <dustin@laverick.org>
2017-10-30 17:56:52 -0500

    WIP: S3 / Axios file uploads

--- app/assets/javascripts/automations.js
+++ app/assets/javascripts/automations.js
@@ -11,24 +11,30 @@ document.addEventListener('turbolinks:load', function() {
         enableDiscount: true
       },
       methods: {
-        // TODO: Code below has uglifier compile issues when deployed to Heroku
-        //
         fileChanged: function(e) {
           var file = e.target.files[0].name,
             signedUrl = e.target.form.dataset.url,
-            data = JSON.parse(e.target.form.dataset.formData),
+            data= JSON.parse(e.target.form.dataset.formData),
             options = {
               headers: {
                 'Content-Type': 'multipart/form-data'
               }
             };
 
-          var formData = new FormData();
-          for ( i in data ) {
-            formData[i] = data[i];
-          }
-          formData['file'] = file;
-          axios.put(signedUrl, formData, options)
+          // TODO: AND NOW DO THIS
+          // We're uploading to the bucket URL, https://touchcard-data-dev.s3.amazonaws.com/
+          // not to the presigned URL
+          // We need to make sure the path + filename are appended
+
+
+          // var formData = new FormData();
+          // for ( i in data ) {
+          //   formData[i] = data[i];
+          // }
+          // formData['file'] = file;
+          debugger;
+          uploadPath = signedUrl + '/' + data.key;
+          axios.put(uploadPath, data, options)
             .then(function(result) {
               debugger;
             })
@@ -36,6 +42,8 @@ document.addEventListener('turbolinks:load', function() {
               debugger;
             });
         }
+
+
       }
     });
 
@@ -46,6 +54,18 @@ document.addEventListener('turbolinks:load', function() {
     // });
   }
 });
+
+
+// const data = new FormData();
+//
+// data.append('action', 'ADD');
+// data.append('param', 0);
+// data.append('secondParam', 0);
+// data.append('file', new Blob(['test payload'], { type: 'text/csv' }));
+//
+// axios.post('http://httpbin.org/post', data);
+
+
 
 
 
