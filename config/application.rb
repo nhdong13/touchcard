498579f643a66c9bc61d64f04cd1186e1678aa27
dixpac <dino.onex@gmail.com>
2017-10-31 15:25:21 +0100

    Move env varable loading to proper place
    Loading env variables was on the wrong place. Loading before
    initalizers.
    Move loading envs to the initalizers as the first initalizer(rails
    executes initalizer by aplpabetical order hence 01_ prefix)

--- config/application.rb
+++ config/application.rb
@@ -30,12 +30,5 @@ module Touchcard
     # TODO Check why this throws error for Rails 5.1.x
     # config.active_record.raise_in_transactional_callbacks = true
     # config.force_ssl = true
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
   end
 end
