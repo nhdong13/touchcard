096f4512de00120ae9be58a31a2110de885ab762
Dustin Laverick <dustin@laverick.org>
2017-10-31 16:56:13 -0500

    WIP - Fix S3 file upload

--- app/assets/javascripts/automations.js
+++ app/assets/javascripts/automations.js
@@ -12,60 +12,39 @@ document.addEventListener('turbolinks:load', function() {
       },
       methods: {
         fileChanged: function(e) {
-          var file = e.target.files[0].name,
-            signedUrl = e.target.form.dataset.url,
-            data= JSON.parse(e.target.form.dataset.formData),
-            options = {
-              headers: {
-                'Content-Type': 'multipart/form-data'
-              }
-            };
-
-          // TODO: AND NOW DO THIS
-          // We're uploading to the bucket URL, https://touchcard-data-dev.s3.amazonaws.com/
-          // not to the presigned URL
-          // We need to make sure the path + filename are appended
-
-
-          // var formData = new FormData();
-          // for ( i in data ) {
-          //   formData[i] = data[i];
-          // }
-          // formData['file'] = file;
-          debugger;
-          uploadPath = signedUrl + '/' + data.key;
-          axios.put(uploadPath, data, options)
-            .then(function(result) {
-              debugger;
+          var file = e.target.files[0];
+          var image_url = '';
+          axios.get('https://touchcard.ngrok.io/aws/sign', {
+            params: {
+              name: file.name,
+              type: file.type
+            }
+          })
+            .then(function (result) {
+              // console.log(result)
+              // debugger;
+              var signedUrl = result.data.signedUrl;
+              image_url = result.data.url;
+              var options = {
+                headers: {
+                  'Content-Type': file.type,
+                  'acl': 'public-read'
+                }
+              };
+              return axios.put(signedUrl, file, options);
             })
-            .catch(function(result) {
-              debugger;
+            .then(function (result) {
+              // console.log(result);
+              // console.log(image_url);
+              // debugger;
+            })
+            .catch(function (err) {
+              // console.log(err);
+              // debugger;
             });
         }
-
-
       }
     });
 
-    // var vueapp = new Vue({
-    //   el: element,
-    //   template: '<App/>',
-    //   components: { App }
-    // });
   }
 });
-
-
-// const data = new FormData();
-//
-// data.append('action', 'ADD');
-// data.append('param', 0);
-// data.append('secondParam', 0);
-// data.append('file', new Blob(['test payload'], { type: 'text/csv' }));
-//
-// axios.post('http://httpbin.org/post', data);
-
-
-
-
-
