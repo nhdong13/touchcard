4470102d354f95b4aab92aa2318cd7407be0e4ec
Dustin Laverick <dustin@laverick.org>
2017-11-09 22:02:39 -0500

    WIP: Merge Dino's form changes & move into automations_controller

--- app/controllers/automations_controller.rb
+++ app/controllers/automations_controller.rb
@@ -1,16 +1,16 @@
 class AutomationsController < BaseController
+  before_action :set_card_order, only: [:edit, :update, :show, :destroy]
 
   def index
-    @card_orders = @current_shop.card_orders.where(archived: false)
+    @card_orders = @current_shop.card_orders.active
   end
 
   def select_type
   end
 
   def new
-    # @types = CardOrder::TYPES
-    @card = @current_shop.card_orders.create
-    # byebug
+    puts params.to_yaml
+    @card_order = @current_shop.card_orders.new
 
     # this is maybe not necessary --- we are using nested attributes
     # @card_side_back = @card.build_card_side_back(is_back: true)
@@ -18,41 +18,64 @@ class AutomationsController < BaseController
     # @filter = Filter.create(card_order: @card)
   end
 
+
   def edit
     puts params.to_yaml
-    @card = CardOrder.find(params[:id])
+    # puts params.to_yaml
+    # @card = CardOrder.find(params[:id])
   end
 
   def show
+    puts params.to_yaml
   end
 
   def update
-    @card = CardOrder.find(params[:id])
-    @card.update(permitted_params)
-    redirect_to action: 'index'
-  end
-
-  def create
     puts params.to_yaml
-    CardOrder.create(type: 'PostSaleOrder', shop: @current_shop);
-    redirect_to action: 'index'
-  end
+    # @card = CardOrder.find(params[:id])
+    # @card.update(permitted_params)
+    # redirect_to action: 'index'
 
-  def destroy
-    @card = CardOrder.find(params[:id])
-    @card.archive
-    if @card.safe_to_destroy?
-      # The CardOrder / CardSide relation is backwards for easy destroy propagation, so this is a workaround
-      @card.transaction do
-        @card.destroy
-        CardSide.find(@card.card_side_back_id).destroy
-        CardSide.find(@card.card_side_front_id).destroy
-      end
+    if @card_order.update(card_order_params)
+      redirect_to @card_order
+    else
+      render :edit
     end
   end
 
+
+  def create
+    puts params.to_yaml
+    # CardOrder.create(type: 'PostSaleOrder', shop: @current_shop);
+    # redirect_to action: 'index'
+
+    # Type could come from as hidden field, param, etc
+    create_params = {type: 'PostSaleOrder'}.merge(card_order_params)
+    @card_order = @current_shop.card_orders.create(create_params)
+    if @card_order.save
+      redirect_to action: 'index', flash: { notice: "Automation successfully created" }
+    else
+      flash[:error] = @card_order.errors.full_messages.join("\n")
+      render :new
+    end
+  end
+
+
+  def destroy
+    @card_order.archive
+    @card_order.destroy_with_sides
+
+    # TODO: Rescue exception
+  # rescue
+    # Catch error from transaction and do something
+  end
+
   private
-  def permitted_params
+
+  def set_card_order
+    @card_order = @current_shop.card_orders.find(params[:id])
+  end
+
+  def card_order_params
     params.require(:card_order).permit(
       :type,
       :enabled,
@@ -63,4 +86,17 @@ class AutomationsController < BaseController
       card_side_back_attributes: [:image]
     )
   end
+
+  # def permitted_params
+  #   params.require(:card_order).permit(
+  #     :type,
+  #     :enabled,
+  #     :discount_exp,
+  #     :discount_pct,
+  #     filter_attributes: [:filter_data],
+  #     card_side_front_attributes: [:image],
+  #     card_side_back_attributes: [:image]
+  #   )
+  # end
+
 end
