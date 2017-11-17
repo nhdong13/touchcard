87ffc1d7c5079831507f3ee8129b8dc105e0a92e
Dustin Laverick <dustin@laverick.org>
2017-11-17 16:05:25 -0500

    Remove unused Vue components. Set up imports for AutomationEditorPack

---
+++ app/javascript/containers/AutomationEditor.js
@@ -0,0 +1,2 @@
+
+export default { text: 'hello world'};
---
+++ app/javascript/packs/AutomationEditorPack.js
@@ -0,0 +1,16 @@
+
+
+import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
+import VueResource from 'vue-resource'
+import AutomationEditor from '../containers/AutomationEditor'
+
+document.addEventListener('turbolinks:load', () => {
+
+
+}
+
+
+onsole.log('AutomationEditorPack.js');
+
+console.log(AutomationEditor);
