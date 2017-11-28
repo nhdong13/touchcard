24c2bfe61999dd7c7a7ffeef539d2e6cfb6f04ba
Dustin Laverick <dustin@laverick.org>
2017-11-28 16:52:09 -0500

    Faster Development - Make Webpack compress JS files + add a bin/fastdevserver option for production JS config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,14 @@
 const environment = require('./environment')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+// environment.plugins.set('UglifyJs', new webpack.optimize.UglifyJsPlugin({
+//   sourceMap: true
+// }))
+
+environment.plugins.set('Compression', new CompressionPlugin({
+  asset: '[path].gz[query]',
+  algorithm: 'gzip',
+  test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
+}))
 
 module.exports = environment.toWebpackConfig()
