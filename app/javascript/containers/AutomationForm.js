8edf768e9ad30180c246e143a1f4adc44699b2b8
Dustin Laverick <dustin@laverick.org>
2017-11-30 18:27:29 -0500

    Coupon on Canvas - Boundary Drag & Resize Works

--- app/javascript/containers/AutomationForm.js
+++ app/javascript/containers/AutomationForm.js
@@ -29,43 +29,80 @@ export default function loadAutomationEditor (element) {
         newFrontImageData: null,
         newBackImage: null,
         newBackImageData: null,
+        canvas: null,
+        originalCanvasHeight: 1275,
+        originalCanvasWidth: 1875,
       };
     },
+    beforeDestroy: function () {
+      window.removeEventListener('resize', this.handleResize)
+    },
     mounted: function() {
-      console.log('Mounted');
+      // console.log('Mounted');
+      // this.$nextTick(function () {
+      // code that assumes this.$el is in-document
+      // });
 
       // create a wrapper around native canvas element (with id="c")
-      let fabricCanvas= new fabric.Canvas('card-editor', { stateful: true });
+      this.canvas = new fabric.Canvas('card-editor', { stateful: true });
       let rect = new fabric.Rect({ left: 100, top: 100, fill: 'red', width: 510, height: 300, hasControls: false, hasBorders: false });
-      fabricCanvas.add(rect);
+      this.canvas.add(rect);
 
 
-      fabricCanvas.on('object:moving', function (e) {
-        var obj = e.target;
-        // if object is too big ignore
-        if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
-          return;
-        }
-        obj.setCoords();
-        // top-left  corner
-        if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
-          obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
-          obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
-        }
-        // bot-right corner
-        if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
-          obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
-          obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
-        }
-      });
+      this.canvas.on('object:moving', this.handleObjectMoved);
 
+      window.addEventListener('resize', this.handleResize);
 
-      this.rect = rect;
+      this.handleResize();
+
+      // document.addEventListener('resize', () => { console.log('RESIZING'); });
+
     },
     // components: {
     //   'card-editor': CardEditor
     // },
     methods: {
+      handleObjectMoved: function(e) {
+        var obj = e.target;
+
+        console.log('x      : ' + obj.left + ' y      :' + obj.top);
+
+        console.log('x bound: ' + obj.getBoundingRect().left + ' y bound:' + obj.getBoundingRect().top);
+
+        // if object is too big ignore
+        // if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
+        //   return;
+        // }
+        // obj.setCoords();
+
+        if (obj.top < 0) {
+          obj.top = 0;
+        }
+        if (obj.left < 0) {
+          obj.left = 0;
+        }
+
+        if (obj.top + obj.height > this.originalCanvasHeight) {
+          obj.top = this.originalCanvasHeight - obj.height;
+        }
+
+        if (obj.left + obj.width > this.originalCanvasWidth) {
+          obj.left = this.originalCanvasWidth - obj.width;
+        }
+
+
+        // Solution from here: https://stackoverflow.com/a/24238960/1181104
+        // but bounding box isn't updating properly after canvas resize:
+
+
+      },
+      handleResize: function() {
+
+        let ratio = (Math.min(this.originalCanvasWidth/2, window.innerWidth)/ 1875) * 0.90;
+
+        this.canvas.setDimensions({ width: this.originalCanvasWidth * ratio, height: this.originalCanvasHeight * ratio });
+        this.canvas.setZoom(ratio);
+      },
       saveAutomation: function() {
         let promises = [];
         if (this.newFrontImage) {
