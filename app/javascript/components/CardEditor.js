e467a4180080131b8a55b9c8c4c9ea015506a1ec
Dustin Laverick <dustin@laverick.org>
2017-12-07 11:57:45 -0500

    Move card editor back into Vue component

--- app/javascript/components/CardEditor.js
+++ app/javascript/components/CardEditor.js
@@ -1,61 +1,95 @@
-// import { fabric }from 'fabric-browseronly'
-//
+import { fabric }from 'fabric-browseronly'
+
 // import 'stylesheets/styles/automations'
+
+
+// function resizeCanvasToDisplaySize(canvas) {
+//   // look up the size the canvas is being displayed
+//   const width = canvas.clientWidth;
+//   const height = canvas.clientHeight;
 //
-//
-// // function resizeCanvasToDisplaySize(canvas) {
-// //   // look up the size the canvas is being displayed
-// //   const width = canvas.clientWidth;
-// //   const height = canvas.clientHeight;
-// //
-// //   // If it's resolution does not match change it
-// //   if (canvas.width !== width || canvas.height !== height) {
-// //     canvas.width = width;
-// //     canvas.height = height;
-// //     return true;
-// //   }
-// //
-// //   return false;
-// // }
-//
-// // Coupon is 510px by 300px
-//
-// let CardEditor = {
-//   // template: '<canvas id="card-editor" class="card-side-canvas" width=625 height=425></canvas>',
-//   data: function() {
-//     return {
-//       rect: null
-//     }
-//   },
-//   mounted: function() {
-//     console.log('Mounted Card Editor');
-//
-//     // create a wrapper around native canvas element (with id="c")
-//     let fabricCanvas= new fabric.Canvas('card-editor');
-//     let rect = new fabric.Rect({ left: 100, top: 100, fill: 'red', width: 170, height: 100, hasControls: false, hasBorders: false });
-//     fabricCanvas.add(rect);
-//
-//     this.rect = rect;
-//
-//     // let canvas = this.$el;
-//     // // canvas.width = window.innerWidth;
-//     // // canvas.height = window.innerHeight;
-//     //
-//     // canvas.width = 800;
-//     // canvas.height = 600;
-//     // // canvas.innerWidth = 800;
-//     // // canvas.innerHeight = 800;
-//
-//     window.rect = rect;
-//     window.editor = this;
-//     // rect.hasControls = false;
-//     // rect.hasBorders = false;
-//
-//
-//     // debugger;
+//   // If it's resolution does not match change it
+//   if (canvas.width !== width || canvas.height !== height) {
+//     canvas.width = width;
+//     canvas.height = height;
+//     return true;
 //   }
-// };
 //
-//
-// export default CardEditor;
-//
+//   return false;
+// }
+
+// Coupon is 510px by 300px
+
+let CardEditor = {
+  template: '<div class="card-editor-container"><canvas id="card-editor-canvas" class="card-side-canvas" width=1875 height=1275></canvas></div>',
+  data: function() {
+    return {
+      rect: null,
+      canvas: null,
+      originalCanvasHeight: 1275,
+      originalCanvasWidth: 1875,
+    }
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  mounted: function() {
+    console.log('CardEditor Mounted');
+    // this.$nextTick(function () {
+    // code that assumes this.$el is in-document
+    // });
+
+    // create a wrapper around native canvas element (with id="c")
+    this.canvas = new fabric.Canvas('card-editor-canvas', { stateful: true });
+    let rect = new fabric.Rect({ left: 100, top: 100, fill: 'red', width: 510, height: 300, hasControls: false, hasBorders: false });
+    this.canvas.add(rect);
+
+
+    this.canvas.on('object:moving', this.handleObjectMoved);
+
+    window.addEventListener('resize', this.handleResize);
+
+    this.handleResize();
+
+    // document.addEventListener('resize', () => { console.log('RESIZING'); });
+
+
+  },
+  methods: {
+    handleObjectMoved: function(e) {
+      // console.log('handleObjectMoved');
+      // console.log('x      : ' + obj.left + ' y      :' + obj.top);
+      // console.log('x bound: ' + obj.getBoundingRect().left + ' y bound:' + obj.getBoundingRect().top);
+
+      var obj = e.target;
+
+      // Solution (modified): https://stackoverflow.com/a/24238960/1181104
+
+      if (obj.top < 0) {
+        obj.top = 0;
+      }
+      if (obj.left < 0) {
+        obj.left = 0;
+      }
+      if (obj.top + obj.height > this.originalCanvasHeight) {
+        obj.top = this.originalCanvasHeight - obj.height;
+      }
+      if (obj.left + obj.width > this.originalCanvasWidth) {
+        obj.left = this.originalCanvasWidth - obj.width;
+      }
+
+    },
+    handleResize: function() {
+      console.log('handleResize');
+
+      let ratio = (Math.min(this.originalCanvasWidth/3, window.innerWidth)/ 1875) * 0.90;
+
+      this.canvas.setDimensions({ width: this.originalCanvasWidth * ratio, height: this.originalCanvasHeight * ratio });
+      this.canvas.setZoom(ratio);
+    },
+  }
+};
+
+
+export default CardEditor;
+
--- app/javascript/containers/AutomationForm.js
+++ app/javascript/containers/AutomationForm.js
@@ -4,10 +4,8 @@ import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(TurbolinksAdapter);
 import axios from 'axios'
 import * as api from '../Api'
-// import CardEditor from '../components/CardEditor'
+import CardEditor from '../components/CardEditor'
 
-
-import { fabric }from 'fabric-browseronly'
 
 export default function loadAutomationEditor (element) {
 
@@ -28,81 +26,16 @@ export default function loadAutomationEditor (element) {
         newFrontImage: null,
         newFrontImageData: null,
         newBackImage: null,
-        newBackImageData: null,
-        canvas: null,
-        originalCanvasHeight: 1275,
-        originalCanvasWidth: 1875,
+        newBackImageData: null
       };
     },
     beforeDestroy: function () {
       window.removeEventListener('resize', this.handleResize)
     },
-    mounted: function() {
-      // console.log('Mounted');
-      // this.$nextTick(function () {
-      // code that assumes this.$el is in-document
-      // });
-
-      // create a wrapper around native canvas element (with id="c")
-      this.canvas = new fabric.Canvas('card-editor', { stateful: true });
-      let rect = new fabric.Rect({ left: 100, top: 100, fill: 'red', width: 510, height: 300, hasControls: false, hasBorders: false });
-      this.canvas.add(rect);
-
-
-      this.canvas.on('object:moving', this.handleObjectMoved);
-
-      window.addEventListener('resize', this.handleResize);
-
-      this.handleResize();
-
-      // document.addEventListener('resize', () => { console.log('RESIZING'); });
-
+    components: {
+      'card-editor': CardEditor
     },
-    // components: {
-    //   'card-editor': CardEditor
-    // },
     methods: {
-      handleObjectMoved: function(e) {
-        var obj = e.target;
-
-        console.log('x      : ' + obj.left + ' y      :' + obj.top);
-
-        console.log('x bound: ' + obj.getBoundingRect().left + ' y bound:' + obj.getBoundingRect().top);
-
-        // if object is too big ignore
-        // if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
-        //   return;
-        // }
-        // obj.setCoords();
-
-        if (obj.top < 0) {
-          obj.top = 0;
-        }
-        if (obj.left < 0) {
-          obj.left = 0;
-        }
-
-        if (obj.top + obj.height > this.originalCanvasHeight) {
-          obj.top = this.originalCanvasHeight - obj.height;
-        }
-
-        if (obj.left + obj.width > this.originalCanvasWidth) {
-          obj.left = this.originalCanvasWidth - obj.width;
-        }
-
-
-        // Solution from here: https://stackoverflow.com/a/24238960/1181104
-        // but bounding box isn't updating properly after canvas resize:
-
-
-      },
-      handleResize: function() {
-
-        let ratio = (Math.min(this.originalCanvasWidth/2, window.innerWidth)/ 1875) * 0.90;
-
-        this.canvas.setDimensions({ width: this.originalCanvasWidth * ratio, height: this.originalCanvasHeight * ratio });
-        this.canvas.setZoom(ratio);
-      },
       saveAutomation: function() {
         let promises = [];
         if (this.newFrontImage) {
