795843bddc45af465b7e2f9dbc874c4d971554b3
Dustin Laverick <dustin@laverick.org>
2018-01-17 15:43:34 -0500

    Add debug route for viewing Lob output in development

--- app/controllers/lob_api_controller.rb
+++ app/controllers/lob_api_controller.rb
@@ -1,5 +1,10 @@
 class LobApiController < ApplicationController
   # This controller is used for rendering a card side to the Lob API, not via routes but ApplicationController::render
-  def show
+  def debug
+    @lob_css_pack_path = LobRenderUtil.lob_css_pack_path
+    @lob_js_pack_path = LobRenderUtil.lob_js_pack_path
+    @postcard = Postcard.last
+    @card_side = @postcard.card_order.card_side_front
+    render :card_side
   end
 end
