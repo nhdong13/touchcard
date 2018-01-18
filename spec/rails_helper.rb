8e75e92461b39929f80ce4402531e2beadee01a0
Dustin Laverick <dustin@laverick.org>
2018-01-18 16:10:04 -0500

    Upgrade FactoryGirl to FactoryBot. SJWs rejoice.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'factory_girl_rails'
+require 'factory_bot'
 require 'simplecov'
 
 SimpleCov.start
@@ -33,8 +33,8 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Configure Factory Girl
-  config.include FactoryGirl::Syntax::Methods
+  # Configure Factory Bot
+  config.include FactoryBot::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
