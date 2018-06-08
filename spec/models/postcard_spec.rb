65913c3b603e2efa0d09651019094399e423a5de
Dustin Laverick <dustin@laverick.org>
2018-06-08 15:28:09 +0200

    Add debug logging

--- spec/models/postcard_spec.rb
+++ spec/models/postcard_spec.rb
@@ -57,6 +57,7 @@ RSpec.describe Postcard, type: :model do
     it "renders_back_no_coupon" do
       output_path =  LobRenderUtil.render_side_png(postcard: postcard, is_front: false)
       expected_png_path = (Rails.root + 'spec/images/expected_back_no_coupon@2x.png').to_s
+      puts "Comparing: \n#{output_path} \nwith: \n#{expected_png_path} "
       expect(FileUtils.compare_file(output_path, expected_png_path)).to be_truthy  # Compare with expected output
       expect(FileUtils.compare_file(output_path, bad_png_path)).to be_falsey  # Compare with bad output (confirms test)
     end
