be56e86543c97d8dcedaa81d94c4b2b68f913fa2
Dustin Laverick <dustin@laverick.org>
2018-08-25 18:42:29 +0100

    Fix up and down migrations for inversion of card_order and card_side relationship

--- db/migrate/20171113072652_add_card_order_reference_to_card_sides.rb
+++ db/migrate/20171113072652_add_card_order_reference_to_card_sides.rb
@@ -2,11 +2,12 @@ class AddCardOrderReferenceToCardSides < ActiveRecord::Migration[5.1]
   def up
     add_reference :card_sides, :card_order, foreign_key: true
     CardOrder.all.each do |card_order|
-      card_order.card_side_front.card_order_id = card_order.id
-      card_order.card_side_back.card_order_id = card_order.id
-      # Make sure there's no out of range errors
-      card_order.discount_exp = card_order.discount_exp.clamp(1, 52) if card_order.discount_exp
-      card_order.save!
+      [card_order.card_side_front_id, card_order.card_side_back_id].map do |card_side_id|
+        card_side = CardSide.find(card_side_id)
+        card_side.card_order_id = card_order.id
+        card_side.save!
+      end
+
     end
   end
 
--- db/migrate/20171113074535_remove_card_sides_from_card_orders.rb
+++ db/migrate/20171113074535_remove_card_sides_from_card_orders.rb
@@ -8,12 +8,13 @@ class RemoveCardSidesFromCardOrders < ActiveRecord::Migration[5.1]
     add_column :card_orders, :card_side_front_id, :integer
     add_column :card_orders, :card_side_back_id, :integer
     CardSide.all.each do |card_side|
+      card_order = CardOrder.find(card_side.card_order_id)
       if card_side.is_back
-        card_side.card_order.card_side_back_id = card_side.id
+        card_order.card_side_back_id = card_side.id
       else
-        card_side.card_order.card_side_front_id = card_side.id
+        card_order.card_side_front_id = card_side.id
       end
-      card_side.save!
+      card_order.save!
     end
   end
 end
