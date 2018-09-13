eed4b06c69bf2953ac1a1c41735c36b1956778d5
Dustin Laverick <dustin@laverick.org>
2018-09-13 15:48:04 +0100

    Upgrade Webpacker

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,11 +3,11 @@ const CompressionPlugin = require('compression-webpack-plugin')
 const merge = require('webpack-merge')
 const customConfig = require('./custom')
 
-// environment.plugins.set('UglifyJs', new webpack.optimize.UglifyJsPlugin({
+// environment.plugins.append('UglifyJs', new webpack.optimize.UglifyJsPlugin({
 //   sourceMap: true
 // }))
 
-environment.plugins.set('Compression', new CompressionPlugin({
+environment.plugins.append('Compression', new CompressionPlugin({
   asset: '[path].gz[query]',
   algorithm: 'gzip',
   test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
