316d071feb1706a4311c95016c351cbac4d3a621
Dustin Laverick <dustin@laverick.org>
2019-03-08 19:11:10 +0000

    add delayed load test

--- spec/lib/lob_render_util_spec.rb
+++ spec/lib/lob_render_util_spec.rb
@@ -39,7 +39,8 @@ RSpec.describe LobRenderUtil do
       expect(FileUtils.compare_file(output_path, bad_png_path)).to be_falsey  # Compare with bad output (confirms test)
     end
 
-    it "renders_card_order_front_with_coupon" do
+
+    it "renders_delayed_load_correctly" do
       # TODO: Render postcard from card_order that failed in database_spec.rb
       #     { shop_id: 858, discount_x: 2, discount_y: 64, discount_pct: -20, discount_exp: 4, image: "https://touchcard-data.s3.amazonaws.com/uploads/b36efa57-a455-4cf5-81f3-b8563678296d/SOURCEvapes_1875x1275_back_2.jpg" },
       #
@@ -48,7 +49,25 @@ RSpec.describe LobRenderUtil do
       #  Expected behaviour with render.js:
       #   networkidle2 - fail
       #   networkidle0 - pass
-      fail "Important test - missing implementation"
+
+
+      # stub_request(:post, postcard_url)
+      #     .to_return(body: File.read("#{Rails.root}/spec/images/background_1.jpg"))
+
+      postcard_url = "https://touchcard-static.s3.amazonaws.com/postcards/test/background_1.jpg"
+      postcard.card_order.front_json['background_url'] = postcard_url
+
+      postcard.discount_exp_at = Time.now + 23.days
+      postcard.discount_code = "XXX-YYY-ZZZ"
+      postcard.discount_pct = -37
+
+      output_path =  LobRenderUtil.render_side_png(postcard: postcard, is_front: true)
+      puts "\nFront render: #{output_path}"
+      mac_compare = FileUtils.compare_file(output_path, (Rails.root + 'spec/images/expected_front_coupon_mac.png').to_s)
+      heroku_compare = FileUtils.compare_file(output_path, (Rails.root + 'spec/images/expected_front_coupon_heroku.png').to_s)
+      gitlab_compare = FileUtils.compare_file(output_path, (Rails.root + 'spec/images/expected_front_coupon_gitlab.png').to_s)
+      expect(mac_compare || heroku_compare || gitlab_compare).to be_truthy  # Compare with `expected_front_coupon[...].png`
+      expect(FileUtils.compare_file(output_path, bad_png_path)).to be_falsey  # Compare with bad output (confirms test)
     end
 
     it "raises_error_on_missing_data" do
