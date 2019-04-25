d7793dfa6d5d737c79362cbec2e3ab8cea1a1e59
Dustin Laverick <dustin@laverick.org>
2019-04-25 16:21:48 +0100

    debug code for test artifacts

--- spec/lib/postcard_render_util_spec.rb
+++ spec/lib/postcard_render_util_spec.rb
@@ -42,7 +42,11 @@ RSpec.describe PostcardRenderUtil do
       mac_compare = FileUtils.compare_file(output_path, (Rails.root + 'spec/images/expected_front_coupon_mac.png').to_s)
       heroku_compare = FileUtils.compare_file(output_path, (Rails.root + 'spec/images/expected_front_coupon_heroku.png').to_s)
 
-      temp_s3_upload(File.basename(output_path), output_path) if ENV['ci'] and not (mac_compare || heroku_compare)
+
+      puts "ENV['ci']: #{ENV['ci']}"
+      result = temp_s3_upload(File.basename(output_path), output_path) if ENV['ci'] and not (mac_compare || heroku_compare)
+      puts "S3 result: #{result}"
+
       expect(mac_compare || heroku_compare ).to be_truthy  # Compare with `expected_front_coupon[...].png`
       expect(FileUtils.compare_file(output_path, bad_png_path)).to be_falsey  # Compare with bad output (confirms test)
     end
