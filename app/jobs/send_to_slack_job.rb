a6676a645f5145b59688016044e1a768d48b4cae
Dustin Laverick <dustin@laverick.org>
2019-08-28 18:34:15 +0100

    remove #weeklywins slack notifications

--- app/jobs/send_to_slack_job.rb
+++ app/jobs/send_to_slack_job.rb
@@ -4,10 +4,6 @@ class SendToSlackJob < ActiveJob::Base
   queue_as :default
 
   def perform(payload)
-    # WeeklyWins
-    secondary_url = "https://hooks.slack.com/services/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"
-    RestClient.post(secondary_url, payload.to_json)
-
     # TeamTouchcard
     RestClient.post(ENV["SLACK_URL"], payload.to_json)
   end
--- lib/slack_notify.rb
+++ lib/slack_notify.rb
@@ -15,10 +15,6 @@ class SlackNotify
     if run_in_background
       SendToSlackJob.perform_later(payload)
     else
-      # WeeklyWins
-      secondary_url = "https://hooks.slack.com/services/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"
-      RestClient.post(secondary_url, payload.to_json)
-
       # TeamTouchcard
       resp = RestClient.post(ENV["SLACK_URL"], payload.to_json)
       resp.code
