2953720a32f105cd6c1f342c36f9bb0d2c05fe3c
nus nick <nick@nustechnology.com>
2021-03-26 10:13:08 +0700

    add country column to csv template

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,4 +1,2 @@
 LOB_API_VER = "2020-02-11"
 TEST_ADDRESS_TO = "adr_8769d8839f5c16c4"
-# BASE_CAMPAIGN_TOOL_URL = 'http://localhost:3001'
-BASE_CAMPAIGN_TOOL_URL = 'https://campaign-tool-dev.herokuapp.com'
