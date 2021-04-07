9c578071b69fed84703ee345044cc30fedcf4358
nus brian <brian@nustechnology.com>
2021-04-07 11:24:05 +0700

    Admin: Provide old password before change password

--- app/admin/ChangePassword.rb
+++ app/admin/ChangePassword.rb
@@ -9,6 +9,7 @@ ActiveAdmin.register_page "Change Password" do
     if params[:new_password].present? &&
       params[:confirmation].present? &&
       params[:new_password] == params[:confirmation] &&
+      current_admin_user.valid_password?(params[:old_password]) &&
       current_admin_user.update(password: params[:new_password])
       flash[:notice] = 'Successfully update password'
     else
