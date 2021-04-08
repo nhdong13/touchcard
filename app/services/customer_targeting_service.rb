284fc690520632d6c5654b00cc197feb11293f1b
nus brian <brian@nustechnology.com>
2021-04-08 13:53:23 +0700

    Filter customers by total spend at shop

--- app/services/customer_targeting_service.rb
+++ app/services/customer_targeting_service.rb
@@ -2,36 +2,55 @@ class CustomerTargetingService
   attr_accessor :current_shop, :orders
   def initialize(shop)
     @current_shop = shop
-    @orders = @current_shop.orders
+    @orders = @current_shop.orders.where.not(customer_id: nil)
   end
 
   def find(accepted_attrs, removed_attrs)
     user_orders_count = orders.group(:customer_id).count
-    accepted_user_ids = []
+    user_spends_count = orders.group(:customer_id).sum(:total_line_items_price)
+    accepted_user_ids = orders.pluck(:customer_id)
     removed_user_ids = []
+
     unless accepted_attrs.present?
       accepted_user_ids = orders.pluck(:customer_id)
     else
-      accepted_attrs[:condition].each_with_index do |condition, i|
-        accepted_user_ids += filter(user_orders_count, condition, accepted_attrs[:value][i].to_i)
+      accepted_attrs[:filter].each_with_index do |filter, i|
+        collection = case filter
+        when "0"
+          user_orders_count
+        when "1"
+          user_spends_count
+        else
+          []
+        end
+        filtered_user_ids = filter_by(collection, accepted_attrs[:condition][i], accepted_attrs[:value][i].to_i)
+        accepted_user_ids = accepted_user_ids.select{|id| filtered_user_ids.index(id).present?}
       end
     end
 
-    removed_attrs[:condition].each_with_index do |condition, i|
-      removed_user_ids += filter(user_orders_count, condition, removed_attrs[:value][i].to_i)
+    removed_attrs[:filter].each_with_index do |filter, i|
+      collection = case filter
+      when "0"
+        user_orders_count
+      when "1"
+        user_spends_count
+      else
+        []
+      end
+      removed_user_ids += filter_by(collection, accepted_attrs[:condition][i], accepted_attrs[:value][i].to_i)
     end if removed_attrs.present?
 
     Customer.find(accepted_user_ids - removed_user_ids)
   end
 
-  def filter orders_count, filter_type, value
+  def filter_by collection, filter_type, value
     case filter_type
       when "0"
-        orders_count.filter{|k,v| v == value}.keys
+        collection.filter{|k,v| v == value}.keys
       when "1"
-        orders_count.filter{|k,v| v > value}.keys
+        collection.filter{|k,v| v > value}.keys
       when "2"
-        orders_count.filter{|k,v| v < value}.keys
+        collection.filter{|k,v| v < value}.keys
       else []
     end
   end
--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,4 +1,4 @@
 LOB_API_VER = "2020-02-11"
 TEST_ADDRESS_TO = "adr_8769d8839f5c16c4"
-FILTER_OPTIONS = [["Number of orders", 0]]
+FILTER_OPTIONS = [["Number of orders", 0], ["Total Spend", 1]]
 CONDITIONS = [["is", 0], ["is greater than", 1], ["is less than", 2]]
