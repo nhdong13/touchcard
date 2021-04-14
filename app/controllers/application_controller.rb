db4aec09eed19a9a06c75ae4f395663a6d16ee99
nus brian <brian@nustechnology.com>
2021-04-14 09:34:56 +0700

    Allow iframe request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :allow_iframe_requests
 
   def download_csv_template
     send_file(
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
       filename: "csv_template.csv",
       type: "text/csv"
     )
+  end
+
+  private
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 end
