64c7563432e7dc1b493fe33ab76202a3bfe46c34
nus brian <brian@nustechnology.com>
2021-06-12 00:15:45 +0700

    Fix testcases fail

--- spec/controllers/postcard_render_controller_spec.rb
+++ spec/controllers/postcard_render_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe PostcardRenderController, type: :feature do  # with :controller h
     end
 
     it "generates html with discount coupon when discount_code, discount_pct, and coupon placement are there" do
-      card_side_div = %(<div id="postcard-render-card-side" data-attributes="{&quot;version&quot;:0,&quot;background_url&quot;:&quot;#{(Rails.root + 'spec/images/background_1.jpg').to_s}&quot;,&quot;discount_x&quot;:376,&quot;discount_y&quot;:56}" data-discount-pct="33" data-discount-exp="01/14/2020" data-discount-code="TEST-ING-YO"></div>)
+      card_side_div = %(<div id="postcard-render-card-side" data-attributes="{&quot;version&quot;:0,&quot;background_url&quot;:&quot;#{(Rails.root + 'spec/images/background_1.jpg').to_s}&quot;,&quot;discount_x&quot;:376,&quot;discount_y&quot;:56}" data-discount-pct="33" data-discount-exp="01/15/2020" data-discount-code="TEST-ING-YO"></div>)
       expected_html = PostcardRenderSpecHelper.postcard_html(card_side_div: card_side_div)
       rendered_html = PostcardRenderController.render_side(postcard: postcard, is_front: true)
       tmp_path = "#{Rails.root}/tmp/test/render_test_complete_coupon.html"
