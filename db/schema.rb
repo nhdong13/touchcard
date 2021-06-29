d4399caed8ac42a5452ba4f6895de58f4ba7ba33
nus nick <nick@nustechnology.com>
2021-06-29 17:27:04 +0700

    implement campaign revert to its previous status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_162836) do
+ActiveRecord::Schema.define(version: 2021_06_29_095706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_162836) do
     t.integer "card_order_parent_id"
     t.boolean "send_continuously", default: true
     t.integer "campaign_status", default: 0
+    t.integer "previous_campaign_status"
   end
 
   create_table "card_sides", id: :serial, force: :cascade do |t|
