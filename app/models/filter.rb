ea9956c76ddb560695325dbee375b0f38f441409
nus brian <brian@nustechnology.com>
2021-07-23 02:59:35 +0700

    Cleanup duplicate filters of the same card order

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -2,4 +2,10 @@ class Filter < ApplicationRecord
   belongs_to :card_order
   serialize :filter_data
   validates :card_order, :filter_data, presence: true
+
+  before_create :cleanup_same_filter
+
+  def cleanup_same_filter
+    Filter.where(card_order_id: self.card_order_id).delete_all
+  end
 end
