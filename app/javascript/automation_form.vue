5cf30cd9ac138316be3894940ef8df6ed2ad5a25
nus nick <nick@nustechnology.com>
2021-07-27 14:02:15 +0700

    fix unstable select end date

--- app/javascript/automation_form.vue
+++ app/javascript/automation_form.vue
@@ -358,6 +358,7 @@
         get: function(){
           const today = new Date()
           today.setDate(today.getDate() - 1)
+          today.setHours(0,0,0,0)
           return {
             to: today
           }
