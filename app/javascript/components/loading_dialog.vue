74e287086286f780fb6f57fc2cdad9d88680b5ba
nus brian <brian@nustechnology.com>
2021-09-05 02:57:53 +0700

    File space

--- app/javascript/components/loading_dialog.vue
+++ app/javascript/components/loading_dialog.vue
@@ -1,25 +1,36 @@
 <template>
-	<modal name="loading-modal" class="loading-modal" :clickToClose="false" width="50" height="50" styles="position: absolute;top: 50%;left: 50%;background: transparent;box-shadow: none;">
-    <img :src="require('../../assets/images/loading.gif')" width="50" height="50" />
+  <modal
+    name="loading-modal"
+    class="loading-modal"
+    :clickToClose="false"
+    width="50"
+    height="50"
+    styles="position: absolute;top: 50%;left: 50%;background: transparent;box-shadow: none;"
+  >
+    <img
+      :src="require('../../assets/images/loading.gif')"
+      width="50"
+      height="50"
+    />
   </modal>
 </template>
 <script>
 export default {
-	name: "LoadingDialog",
-	props: {
-		modalDisplay: {
-			type: Boolean,
-			required: true
-		},
-	},
-	watch: {
-		modalDisplay: function(val) {
-			if (val) {
-				this.$modal.show('loading-modal')
-			} else {
-				this.$modal.hide('loading-modal')
-			}
-		}
-	}
-}
+  name: "LoadingDialog",
+  props: {
+    modalDisplay: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  watch: {
+    modalDisplay: function (val) {
+      if (val) {
+        this.$modal.show("loading-modal");
+      } else {
+        this.$modal.hide("loading-modal");
+      }
+    },
+  },
+};
 </script>
