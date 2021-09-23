385ad2f77f5e817e7a049825c1ed53e865e0eb21
nus brian <brian@nustechnology.com>
2021-09-23 14:42:42 +0700

    Refactor

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,9 +4,8 @@ class LineItem < ApplicationRecord
   validates :order, :shopify_id, :name, presence: true
   validates :shopify_id, uniqueness: true
 
-  def self.from_shopify!(order, line_item)
-    attrs = line_item.attributes.with_indifferent_access
-    select_attrs = attrs.slice(
+  def self.from_shopify!(order, shopify_line_item)
+    attrs = shopify_line_item.attributes.with_indifferent_access.slice(
       :fulfillable_quantity,
       :fulfillment_service,
       :fulfillment_status,
@@ -24,13 +23,13 @@ class LineItem < ApplicationRecord
       :gift_card,
       :taxable,
       :total_discount
-    ).merge(order: order, shopify_id: line_item.id)
-    db_line_item = LineItem.find_by(shopify_id: line_item.id)
-    if db_line_item.present?
-      db_line_item.update!(select_attrs)
+    ).merge(order: order, shopify_id: shopify_line_item.id)
+    line_item = LineItem.find_by(shopify_id: line_item.id)
+    if line_item.present?
+      line_item.update!(attrs)
     else
-      db_line_item = create!(select_attrs)
+      line_item = create!(attrs)
     end
-    db_line_item
+    line_item
   end
 end
