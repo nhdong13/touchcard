84eb1fad773825631f63acc27e4dbbf2589ba115
nus nick <nick@nustechnology.com>
2021-10-04 11:00:55 +0700

    refractor code of browser_refresh

--- app/assets/javascripts/shopify_app.js
+++ app/assets/javascripts/shopify_app.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   var data = document.getElementById('shopify-app-init').dataset;
-  var AppBridge = window['app-bridge'];
   var createApp = AppBridge.default;
   window.app = createApp({
     apiKey: data.apiKey,
     shopOrigin: data.shopOrigin,
   });
-
-  var actions = AppBridge.actions;
   var TitleBar = actions.TitleBar;
   TitleBar.create(app, {
     title: data.page,
@@ -16,8 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('turbolinks:load', () => {
   var data = document.getElementById('shopify-app-init').dataset;
-  var AppBridge = window['app-bridge'];
-  var actions = AppBridge.actions;
   var History = actions.History;
   const history = History.create(app);
   history.dispatch(History.Action.REPLACE, data.path);
