81a97658d5f286a31360afdac34a644b2fb52434
nus brian <brian@nustechnology.com>
2021-10-12 17:50:37 +0700

    Prevent fail

--- app/jobs/prepare_postcard_job.rb
+++ app/jobs/prepare_postcard_job.rb
@@ -6,10 +6,10 @@ class PreparePostcardJob < ActiveJob::Base
     shop = order.shop
     errors = []
     errors.push("no customer") unless order.customer
-    errors.push("no default address") unless order.customer.default_address
-    errors.push("no street in address") unless order.customer.default_address.address1.present?
-    default_address = order.customer.default_address
-    international = default_address.country_code != "US"
+    errors.push("no default address") unless order.customer&.default_address
+    errors.push("no street in address") unless order.customer&.default_address&.address1&.present?
+    # default_address = order.customer.default_address
+    # international = default_address.country_code != "US"
     return if errors.present?
     post_sale_orders = shop.card_orders.where(enabled: true, type: "PostSaleOrder").sending
     post_sale_orders.each{|post_sale_order| post_sale_order.prepare_for_sending(order) }
