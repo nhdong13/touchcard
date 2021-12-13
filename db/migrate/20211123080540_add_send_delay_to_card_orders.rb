4f505d3b98c63602dbd2aa9b434b1b3eba72130f
nus brian <brian@nustechnology.com>
2021-12-14 05:15:29 +0700

    Fix migration

--- db/migrate/20211123080540_add_send_delay_to_card_orders.rb
+++ db/migrate/20211123080540_add_send_delay_to_card_orders.rb
@@ -1,6 +1,7 @@
 class AddSendDelayToCardOrders < ActiveRecord::Migration[6.1]
   def change
-    change_column_default(:card_orders, :send_delay, from: 2, to: 0)
+    # change_column_default(:card_orders, :send_delay, from: 2, to: 0)
+    add_column :card_orders, :send_delay, :integer, default: 0
     #Change send delay from weeks to days
     CardOrder.where.not(send_delay: 0).find_each do |campaign|
       campaign.send_delay = campaign.send_delay * 7
--- db/migrate/20211213074958_add_tmp_send_delay.rb
+++
@@ -1,5 +0,0 @@
-class AddTmpSendDelay < ActiveRecord::Migration[6.1]
-  def change
-    add_column :card_orders, :send_delay, :integer, default: 0
-  end
-end
