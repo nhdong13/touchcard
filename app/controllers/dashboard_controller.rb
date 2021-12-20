3ef21737596132a1bc1f0ff6fb12bf572eb1851a
nus nick <nick@nustechnology.com>
2021-12-20 17:14:47 +0700

    Improve UI dashboard dropdown

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,13 @@ class DashboardController < BaseController
       @result = PostcardsService.new(@current_shop, postcards_search_params).all
       @postcards = @result[:postcards]
       @postcards_with_paging = @result[:postcards_with_paging]
-      
+
+      campaigns_have_postcards = @current_shop.card_orders.have_postcards.order(:campaign_name)
+      @campaigns_for_dropdown = campaigns_have_postcards.active.pluck(:campaign_name, :id).uniq + 
+                                campaigns_have_postcards.unactive.pluck(:campaign_name, :id).uniq.each { |array| 
+                                  array[0] = "(Deleted) #{array[0]}" 
+                                }
+
       respond_to do |format|
         format.html { render :index }
         format.json { render json: { 
